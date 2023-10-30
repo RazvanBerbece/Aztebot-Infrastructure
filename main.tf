1c2fae1068272b190f7671989f610e3870d55f5f
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2023-10-30 23:40:08 +0000

    Add GCloud infra base

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("keys/aztebot-403621-15e70c5e43f9.json")
+
+  project = "aztebot-403621"
+  region  = "europe-west2"
+  zone    = "europe-west2-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
