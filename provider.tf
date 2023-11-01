b66a538e78a658c00f27fa9e16176ebeb3443434
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2023-11-01 01:35:03 +0000

    Add env setup step in tf quality check action

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("keys/aztebot-403621-15e70c5e43f9.json")
+  credentials = file("keys/gcloud_service_account.json")
 
   project = "aztebot-403621"
   region  = "europe-west2"
