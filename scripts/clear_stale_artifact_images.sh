a4760482b64af50c5aa91f4beefc376d5e18463e
Razvan-Antonio Berbece <razvan.berbece@hotmail.com>
2024-05-25 16:56:03 +0100

    finishes working on AR cleaner script

--- scripts/clear_stale_artifact_images.sh
+++ scripts/clear_stale_artifact_images.sh
@@ -6,6 +6,8 @@
 #
 
 # Variables
+dry_run=1 # 0=false (submit deletion requests), 1=true (don't perform any deletions)
+
 image_limit_per_service=1
 
 previous_image_name=""
@@ -14,10 +16,18 @@ current_image_count=0
 
 deleted_count=0
 
+# Announce purpose of run (dry-run or actual run)
+if (( dry_run == 1 )); then
+    echo "Note that this is a dry-run and that no resources will be deleted."
+else
+    echo "Note that this is an actual workflow execution and that resources *may* be deleted."
+fi
+
 # Retrieve a list of all the published images and their metadata
+artifact_registry_result_lines=$(gcloud artifacts docker images list europe-west2-docker.pkg.dev/aztebot-403621/aztebot-docker-ar)
+
 current_header_count=0
-gcloud artifacts docker images list europe-west2-docker.pkg.dev/aztebot-403621/aztebot-docker-ar | while read line 
-do
+while read line; do
     # Skip over header lines
     if (( current_header_count < 1 )); then
         current_header_count=$(( current_header_count+1 ))
@@ -45,11 +55,14 @@ do
         if [ "$current_image_name" = "$previous_image_name" ]; then 
             # And the number of duplicates for given image is already greater than the limit 
             if (( current_image_count >= image_limit_per_service )); then
-                # Then delete the current digest
-                deleted_count=$(( deleted_count+1 ))
+                # Then delete the current published image given its ID (name & digest hash)
                 image_id="$current_image_name@$digest"
-                echo "TO DELETE: $image_id"
-                # gcloud artifacts docker images delete europe-west2-docker.pkg.dev/aztebot-403621/aztebot-docker-ar/IMAGE_NAME@DIGEST
+                # Only send delete requests if NOT in a dry-run
+                if (( dry_run == 0 )); then
+                    res=$(gcloud artifacts docker images delete europe-west2-docker.pkg.dev/aztebot-403621/aztebot-docker-ar/$image_id --async --quiet)
+                fi
+                deleted_count=$(( deleted_count+1 ))
+                echo "Marked for deletion: $image_id"
             fi
         else
             # Moved from a service image to another (e.g. aztebot to aztemusic)
@@ -60,6 +73,12 @@ do
     current_image_count=$(( current_image_count+1 ))
     previous_image_name=$current_image_name
 
-done
+done <<< "$artifact_registry_result_lines"
 
-echo "Deleted a total of $deleted_count redundant Docker images."
+# Announce workflow status at script completion time
+echo "Submitted a total of $deleted_count redundant Docker images for deletion."
+if (( dry_run == 0 )); then
+    echo "Check the operation status in Artifact Registry (https://console.cloud.google.com/artifacts?cloudshell=true&project=aztebot-403621)."
+else
+    echo "Dry-run complete. No resources were deleted in GCP."
+fi
