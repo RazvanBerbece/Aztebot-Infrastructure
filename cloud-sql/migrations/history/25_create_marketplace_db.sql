b9c893e460e91fd7ce29968377d688e86532f161
Razvan-Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-01 15:51:05 +0100

    REFACTOR - Removes a migration file

--- cloud-sql/migrations/history/25_create_marketplace_db.sql
+++
@@ -1,8 +0,0 @@
--- +migrate Up
-CREATE DATABASE azteMarketDb;
-
--- Update the character set and collation for the database
-ALTER DATABASE azteMarketDb CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- +migrate Down
-DROP DATABASE IF EXISTS azteMarketDb;
