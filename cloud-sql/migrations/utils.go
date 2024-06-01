8642c25fd556a1949d96223bd57cf41bd797256e
Razvan-Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-01 17:17:55 +0100

    REFACTOR - Removes some redundant code

--- cloud-sql/migrations/utils.go
+++ cloud-sql/migrations/utils.go
@@ -6,12 +6,3 @@ type DbMigrationTarget struct {
 	ConnString string
 	Fms        *migrate.FileMigrationSource
 }
-
-func StringInSlice(target string, slice []string) bool {
-	for _, str := range slice {
-		if str == target {
-			return true
-		}
-	}
-	return false
-}
