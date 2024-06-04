b600bc8e96d3423a97edb4467ce7dbe7bd9776c1
Razvan-Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-04 23:48:23 +0100

    makes the funds column a float

--- cloud-sql/migrations/history/aztemarket/4_add_wallets_table.sql
+++ cloud-sql/migrations/history/aztemarket/4_add_wallets_table.sql
@@ -4,7 +4,7 @@ USE azteMarketDb;
 CREATE TABLE IF NOT EXISTS Wallets (
   userId                VARCHAR(255) NOT NULL,
   id                    VARCHAR(255) NOT NULL,
-  funds                 VARCHAR(255) NOT NULL,
+  funds                 float(5) NOT NULL,
   inventory             VARCHAR(4096) NOT NULL,
   PRIMARY KEY (userId)
 );
