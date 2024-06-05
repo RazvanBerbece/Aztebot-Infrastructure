e2822bd9cdcfe9d416f097dbb99434989cf1e52b
Razvan-Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-05 19:49:54 +0100

    changes datatype for funds column in Wallet

--- cloud-sql/migrations/history/aztemarket/4_add_wallets_table.sql
+++ cloud-sql/migrations/history/aztemarket/4_add_wallets_table.sql
@@ -4,7 +4,7 @@ USE azteMarketDb;
 CREATE TABLE IF NOT EXISTS Wallets (
   userId                VARCHAR(255) NOT NULL,
   id                    VARCHAR(255) NOT NULL,
-  funds                 float(5) NOT NULL,
+  funds                 DECIMAL(13, 2),
   inventory             VARCHAR(4096) NOT NULL,
   PRIMARY KEY (userId)
 );
