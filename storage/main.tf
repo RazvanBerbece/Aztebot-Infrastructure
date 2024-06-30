0dbc367f29d23cf95f425e9627b6b5e62c1bd2a4
Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-30 20:25:31 +0100

    creates storage bucket for to-be per-guild config files

--- storage/main.tf
+++ storage/main.tf
@@ -10,3 +10,15 @@ resource "google_storage_bucket" "tfstate-bucket" {
     enabled = true
   }
 }
+
+resource "google_storage_bucket" "discord-appdata-bucket" {
+  name                        = "bucket-discord-appdata"
+  force_destroy               = true
+  location                    = "EUROPE-SOUTHWEST1"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
